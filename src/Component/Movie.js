79901a716acbc3ecad07cf159bc9c153d6f0f43b
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-02-23 17:03:16 +0530

    movies

--- src/Component/Movie.js
+++ src/Component/Movie.js
@@ -4,9 +4,9 @@ import { motion } from "framer-motion";
 function Movie({ movie }) {
   return (
     <motion.div
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      exit={{ opacity: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, scale: 0 }}
+      exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 2 }}
       layout
     >
